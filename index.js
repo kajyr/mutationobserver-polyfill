33932bce3097773ea8b67942054d702454864dd3
Carlo Panzi <carlo.panzi@gmail.com>
2017-09-04 10:41:47 +0200

    Removed global return

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-if (window.MutationObserver != null) { return; }
-
-class MutationObserver {
-
-	constructor(callBack) {
-		this.callBack = callBack;
-	}
-
-	observe(element, options) {
-		this.element = element;
-		return this.interval = setInterval(() => {
-				
-			const html = this.element.innerHTML;
-
-			if (html !== this.oldHtml) {
-				this.oldHtml = html;
-				return this.callBack.apply(null);
+if (!window.MutationObserver) { 
+	
+		class MutationObserver {
+	
+			constructor(callBack) {
+				this.callBack = callBack;
+			}
+	
+			observe(element, options) {
+				this.element = element;
+				return this.interval = setInterval(() => {
+						
+					const html = this.element.innerHTML;
+	
+					if (html !== this.oldHtml) {
+						this.oldHtml = html;
+						return this.callBack.apply(null);
+					}
+				}
+	
+				, 200);
+			}
+			
+			disconnect() {
+				return clearInterval(this.interval);
 			}
 		}
-
-		, 200);
-	}
 	
-	disconnect() {
-		return clearInterval(this.interval);
+	
+		window.MutationObserver = MutationObserver;
+	
 	}
-}
-
-
-window.MutationObserver = MutationObserver;
